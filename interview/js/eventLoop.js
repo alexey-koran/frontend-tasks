53b4194240d24213e7ec9763a73995c4d99afa1a
Alexey Koran <alexey.koran@gmail.com>
2024-09-13 12:29:14 +0200

    feat: add eventLoop js interview task

---
+++ interview/js/eventLoop.js
@@ -0,0 +1,17 @@
+console.log(1);
+
+setTimeout(() => console.log(2));
+
+Promise.resolve().then(() => console.log(3));
+
+await Promise.resolve().then(() => console.log(4));
+
+Promise.resolve().then(() => setTimeout(() => console.log(5)));
+
+await Promise.resolve().then(() => console.log(6));
+
+Promise.resolve().then(() => console.log(7));
+
+setTimeout(() => console.log(8));
+
+console.log(9);
